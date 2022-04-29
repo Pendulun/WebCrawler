e1c88f1f4885cfb076a8eebec582d39310cda1be
Daniel Campos <danielcampos@dcc.ufmg.br>
2022-04-29 15:16:39 -0300

    Utils

--- utils.py
+++ utils.py
@@ -4,3 +4,21 @@ def printErrorMessageAndExitWithErrorCode(exceptionRaised, errorCode):
 
 def printJoinedErrorMessage(exceptionRaised):
     print(f"{type(exceptionRaised)} ","".join(exceptionRaised.args))
+
+def getHostAndResourcesFromLink(link):
+    host = getHostOfLink(link)
+    resources = getResourcesFromLink(link)
+    return host, resources
+
+def getHostOfLink(link):
+    """
+    Assumes the link is in the format:
+    https://host/resources
+    """
+    return link.split("/")[2]
+
+def getResourcesFromLink(link):
+    return f"/{'/'.join(link.split('/')[3:])}"
+
+def threadOfHost(numThreads, host):
+    return abs(hash(host)%numThreads)
